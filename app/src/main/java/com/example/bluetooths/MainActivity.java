e1269a30a17edb76171d56cf7fae4521374f26c2
Vinutoun <48645626+Vinutoun@users.noreply.github.com>
2020-03-11 13:00:58 +1100

    Uploading the app

---
+++ app/src/main/java/com/example/bluetooths/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.bluetooths;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button scan;
+    ListView list_view;
+    //CheckBox enable_bt;
+    CheckBox enable_bt;
+    ArrayList<String> stringArrayList = new ArrayList<String>();
+    ArrayAdapter<String> arrayAdapter;
+    BluetoothAdapter myAdapter = BluetoothAdapter.getDefaultAdapter();
+    private int androidVersion;
+    /**
+    private int REQUEST_ID=1;
+    private void requestPermissions(){
+        if (androidVersion >= 23){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,}, REQUEST_ID);
+        }
+    }
+    */
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        scan = (Button) findViewById(R.id.scan);
+        list_view = (ListView) findViewById(R.id.list_view);
+        enable_bt= (CheckBox)findViewById(R.id.on);
+
+        if (myAdapter==null){
+            Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_SHORT).show();
+        }
+
+        if (myAdapter.isEnabled()){
+            enable_bt.setChecked(true);
+        }
+
+        enable_bt.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked){
+                    myAdapter.disable();
+                    Toast.makeText(MainActivity.this, "Turned off", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Intent intentOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(intentOn, 0);
+                    Toast.makeText(MainActivity.this, "Turned on", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                myAdapter.startDiscovery();
+
+
+            }
+        });
+
+        IntentFilter intentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(myReceiver,intentFilter);
+
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,stringArrayList);
+        list_view.setAdapter((arrayAdapter));
+
+
+
+    }
+
+    BroadcastReceiver myReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action))
+            {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                stringArrayList.add(device.getName());
+                arrayAdapter.notifyDataSetChanged();
+
+            }
+
+        }
+    };
+}
