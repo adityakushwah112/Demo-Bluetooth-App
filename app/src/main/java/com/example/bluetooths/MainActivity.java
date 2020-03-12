88a9a598a232626f4ca35de7385037e10f05ba34
Aditya Singh Kushwah <dropmail.ask@gmail.com>
2020-03-12 13:24:14 +1100

    Test commit

--- app/src/main/java/com/example/bluetooths/MainActivity.java
+++ app/src/main/java/com/example/bluetooths/MainActivity.java
@@ -1,52 +1,32 @@
 package com.example.bluetooths;
 
-import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.le.BluetoothLeScanner;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
-
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    int REQUEST_ENABLE_BT = 1;
+
     Button scan;
     ListView list_view;
     CheckBox enable_bt;
     ArrayList<String> stringArrayList = new ArrayList<String>();
     ArrayAdapter<String> arrayAdapter;
     BluetoothAdapter myAdapter = BluetoothAdapter.getDefaultAdapter();
-    private int androidVersion;
-    /**
-    private int REQUEST_ID=1;
-    private void requestPermissions(){
-        if (androidVersion >= 23){
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,}, REQUEST_ID);
-        }
-    }
-    */
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -105,3 +85,37 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 }
+
+/*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Verify if bluetooth is supported
+        BluetoothAdapter myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(myBluetoothAdapter==null){
+            // Device does not support Bluetooth
+        }
+        else {
+            if(!myBluetoothAdapter.isEnabled()) {
+                // Code for Bluetooth Enable
+                Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == REQUEST_ENABLE_BT) {
+            if(resultCode==RESULT_OK){
+                // Bluetooth is enabled
+            }else if(requestCode == RESULT_CANCELED)
+            {
+                // Bluetooth enabling is cancelled
+            }
+        }
+    }
+}
+*/
